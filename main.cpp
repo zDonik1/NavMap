dee8679775c505a69899aeb2aba052b6a73ecd39
zDonik <tokhirovdoniyor@gmail.com>
2019-11-28 20:44:45 +0500

    Removed test and finished application

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
-#include "adjlistnondirgraph.h"
+#include <fstream>
+#include "stack.h"
+#include "map.h"
 
 void printPath(Stack<int> l_path)
 {
@@ -16,22 +18,55 @@ void printPath(Stack<int> l_path)
     std::cout << std::endl;
 }
 
+void printCharMap(const CharMap &l_char_map)
+{
+    for (size_t i = 0; i < l_char_map.back().size(); ++i) {
+        for (size_t j = 0; j < l_char_map.size(); ++j) {
+            std::cout << l_char_map.at(j).at(l_char_map.back().size() - 1 - i);
+        }
+
+        std::cout << std::endl;
+    }
+}
+
+void handleIO(const Map &map)
+{
+    while (true) {
+        system("cls");
+        printCharMap(map.getCharMap());
+        std::cout << std::endl;
+
+        std::cout << "Enter starting and ending positions, or '~' to quit" << std::endl;
+        char in_ch1;
+        std::cin >> in_ch1;
+        if (in_ch1 == '~') {
+            break;
+        }
+
+        char in_ch2;
+        std::cin >> in_ch2;
+        std::cout << std::endl;
+
+        printCharMap(map.getCharMapWithPath(in_ch1, in_ch2));
+        std::cout << std::endl;
+
+        system("pause");
+    }
+}
+
 int main()
 {
-    // test graph
-    AdjListNonDirGraph<5> graph;
-    graph.insert_edge(0, 2, 2);
-    graph.insert_edge(0, 4, 6);
-    graph.insert_edge(1, 2, 7);
-    graph.insert_edge(2, 4, 1);
-    graph.insert_edge(3, 4, 4);
-    graph.finalize();
+    Map map;
+    std::ifstream ifs("maps/map1.map");
+    if (!ifs.is_open()) {
+        std::cout << "Couldn't load map" << std::endl;
+    }
 
-    auto path = graph.getShortestPath(0, 3);
-    printPath(path);
-    // end test
+    map.setCharMapEncoding('x', '@', 'O');
+    map.load(ifs);
+    ifs.close();
 
-
+    handleIO(map);
 
     return 0;
 }
